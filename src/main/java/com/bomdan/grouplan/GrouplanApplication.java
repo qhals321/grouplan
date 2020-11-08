e321ef754b3ea4d6b8831716bef128eaa1c87119
bomin <bomin_93@naver.com>
2020-11-08 21:48:01 +0900

    Initial commit

---
+++ src/main/java/com/bomdan/grouplan/GrouplanApplication.java
@@ -0,0 +1,13 @@
+package com.bomdan.grouplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrouplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bomdan/grouplan/GrouplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomdan.grouplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
